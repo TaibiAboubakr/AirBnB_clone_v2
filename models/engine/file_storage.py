220ed030a0d1dc7f068e179a064db21dd4e9c953
aboubakr <c.aboubakr@gmail.com>
2023-12-18 23:47:08 +0100

    update file_storage.py , add delete method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
+        if cls is not None:
             return FileStorage.__objects
 
     def new(self, obj):
