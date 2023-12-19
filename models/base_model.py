3b2f40aa350b034a9369e300a1736f026156f91d
aboubakr <c.aboubakr@gmail.com>
2023-12-19 19:07:17 +0100

    update base_model.py and city.py adding attributes for SQLAlchemy module

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
     updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
@@ -19,7 +19,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -37,6 +36,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -48,3 +48,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        from models import storage
+        storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = "cities"
+    name = Column(String(60), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'),
+                      ondelete='CASCADE', nullable=False)
+    state = relationship('State', back_populates='City')
+
     state_id = ""
     name = ""
