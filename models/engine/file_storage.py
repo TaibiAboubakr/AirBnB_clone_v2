5e2b057d2560ec5c654c9ab8a563ff75bac93658
aboubakr <c.aboubakr@gmail.com>
2023-12-19 18:27:34 +0100

    update file_storage.py , update 'all' method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            if not isinstance(cls, type):
+                raise TypeError("cls must be a class")
+            filtered_objects = {k: v for k, v in FileStorage.__objects.items()
+                                if isinstance(v, cls)}
+            return filtered_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
