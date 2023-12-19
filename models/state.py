87bcacc279ad62ef754e504987a28b0ad045dd76
nisselhbb <nissrineelhobbi@gmail.com>
2023-12-19 19:39:36 +0100

    updating state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+rom sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """a state class that inherits from BaseModel"""
+    if getenv ("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all , delete")
+    else:
+        name = ""
