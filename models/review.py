07a5120c7acadef9bf22b23b81409d94c16960c5
nisselhbb <nissrineelhobbi@gmail.com>
2023-12-20 21:43:57 +0100

    updating review

--- models/review.py
+++ models/review.py
@@ -12,7 +12,7 @@ class Review(BaseModel):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "reviews"
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     place_id = ""
     user_id = ""
     text = ""
