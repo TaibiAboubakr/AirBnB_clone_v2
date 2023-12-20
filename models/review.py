7f4bb74bfc9bea50dc4d5d5b9a990a921c70c767
nisselhbb <nissrineelhobbi@gmail.com>
2023-12-20 21:46:34 +0100

    updating review

--- models/review.py
+++ models/review.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from os import getenv
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Text, ForeignKey
 from sqlalchemy.orm import relationship
 
