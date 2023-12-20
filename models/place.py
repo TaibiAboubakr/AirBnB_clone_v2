312f4cec1b32cbbfae7503fda86148ffb5663f19
aboubakr <c.aboubakr@gmail.com>
2023-12-20 15:27:22 +0100

    update user.py class adding attributes for SQLAlchemy module

--- models/place.py
+++ models/place.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(int, nullable=False, default=0)
+        number_bathrooms = Column(int, nullable=False, default=0)
+        max_guest = Column(int, nullable=False, default=0)
+        price_by_night = Column(int, nullable=False, default=0)
+        latitude = Column(float)
+        longitude = Column(float)
+        cities = relationship('City', back_populates='places',
+                              cascade='all, delete')
+        users = relationship('User', back_populates='places',
+                             cascade='all, delete')
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
