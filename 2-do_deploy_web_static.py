8a2852244af96a9e5f824855c845539455967452
aboubakr <c.aboubakr@gmail.com>
2024-01-10 22:34:48 +0100

    update the file 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ import os
 env.hosts = ['34.229.66.77', '18.209.225.222']
 env.user = "ubuntu"
 
+
 def do_deploy(archive_path):
     """distributes an archive to your web servers."""
     if not os.path.exists(archive_path):
@@ -15,13 +16,16 @@ def do_deploy(archive_path):
 
     if put(archive_path, '/tmp/').failed:
         return False
-    if run(f'sudo mkdir -p /data/web_static/releases/{archive_basename}/').failed:
+    if run(f'sudo mkdir -p /data/web_static/releases/\
+           {archive_basename}/').failed:
         return False
     if run(f'sudo tar -xzf /tmp/{archive_name}\
            -C /data/web_static/releases/{archive_basename}').failed:
         return False
     if run(f'sudo rm -rf /tmp/{archive_name}').failed:
         return False
+    run(f"sudo mv /data/web_static/releases/{archive_basename}/web_static/*\
+        /data/web_static/releases/{archive_basename}/")
     if run('sudo rm -rf /data/web_static/current').failed:
         return False
     if run('ln -sf /data/web_static/releases/{archive_basename}\
