e7b7b09075f13fd7f9de6caadd7a1c228b56c725
aboubakr <c.aboubakr@gmail.com>
2024-01-10 22:29:38 +0100

    update the file 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,27 +4,27 @@ from fabric.api import env, put, run
 import os
 """module contains do_deploy function"""
 env.hosts = ['34.229.66.77', '18.209.225.222']
-
+env.user = "ubuntu"
 
 def do_deploy(archive_path):
     """distributes an archive to your web servers."""
-    if os.path.isfile(archive_path) is False:
+    if not os.path.exists(archive_path):
         return False
     archive_name = str(archive_path).split('/')[-1]
     archive_basename = str(archive_name).split('.')[0]
 
-    if put(archive_path, f'/tmp/{archive_name}').failed:
+    if put(archive_path, '/tmp/').failed:
         return False
-    if run(f'mkdir /data/web_static/releases/{archive_basename}/').failed:
+    if run(f'sudo mkdir -p /data/web_static/releases/{archive_basename}/').failed:
         return False
-    if run(f'tar -xzf /tmp/{archive_name}\
+    if run(f'sudo tar -xzf /tmp/{archive_name}\
            -C /data/web_static/releases/{archive_basename}').failed:
         return False
-    if run(f'rm - rf /tmp/{archive_name}').failed:
+    if run(f'sudo rm -rf /tmp/{archive_name}').failed:
         return False
-    if run(f'rm /data/web_static/current').failed:
+    if run('sudo rm -rf /data/web_static/current').failed:
         return False
-    if run(f'ln -sf /data/web_static/releases/{archive_basename}\
+    if run('ln -sf /data/web_static/releases/{archive_basename}\
                  /data/web_static/current').failed:
         return False
     return True
