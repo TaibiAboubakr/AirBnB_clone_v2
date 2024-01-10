7032d675771c5cd85184a36dc2f9a9710dc40917
aboubakr <c.aboubakr@gmail.com>
2024-01-10 20:53:48 +0100

    update the file 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,29 +6,25 @@ import os
 env.hosts = ['34.229.66.77', '18.209.225.222']
 
 
-@task
 def do_deploy(archive_path):
     """distributes an archive to your web servers."""
     success = True
-    current_directory = os.getcwd()
-    full_archive_path = os.path.join(current_directory, archive_path)
-    if not os.path.exists(full_archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
     archive_name = str(archive_path).split('/')[-1]
-    archive_basename = str(archive_path).split('/')[-1].split('.')[0]
+    archive_basename = str(archive_name).split('.')[0]
 
-    for host in env.hosts:
-        result = put(archive_path, remote=f'/tmp/', host=host)
-        success = success and result.succeeded
-        result = run(f'mkdir /data/web_static/releases/{archive_basename}')
-        success = success and result.succeeded
-        result = run(f'tar -xzf /tmp/{archive_name}\
-            -C /data/web_static/releases/{archive_basename}')
-        success = success and result.succeeded
-        result = run(f'rm - rf /tmp/{archive_name}')
-        success = success and result.succeeded
-        result = run(f'rm /data/web_static/current')
-        success = success and result.succeeded
-        result = run(f'ln -sf /data/web_static/releases/{archive_basename}\
-                     /data/web_static/current')
+    result = put(archive_path, remote=f'/tmp/')
+    success = success and result.succeeded
+    result = run(f'mkdir /data/web_static/releases/{archive_basename}')
+    success = success and result.succeeded
+    result = run(f'tar -xzf /tmp/{archive_name}\
+        -C /data/web_static/releases/{archive_basename}')
+    success = success and result.succeeded
+    result = run(f'rm - rf /tmp/{archive_name}')
+    success = success and result.succeeded
+    result = run(f'rm /data/web_static/current')
+    success = success and result.succeeded
+    result = run(f'ln -sf /data/web_static/releases/{archive_basename}\
+                 /data/web_static/current')
     return success
