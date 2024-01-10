65379f27281e677d77e1e60825c61440da3b9236
aboubakr <c.aboubakr@gmail.com>
2024-01-10 19:07:30 +0100

    update the file 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-from invoke import task
-from fabric import Connection
+from fabric.api import local
 from datetime import datetime
 import os
 """module contains do_pack function"""
 
 
-@task
-def do_pack(c):
+def do_pack():
     """Generate a .tgz archive from the contents of the web_static folder."""
     current_directory = os.getcwd()
     directory_path = os.path.join(current_directory, "versions")
     if not os.path.exists(directory_path):
-        c.run("mkdir -p versions")
+        local("mkdir -p versions")
     timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     archive_name = "web_static_{}.tgz".format(timestamp)
     print(timestamp)
     archive_path = os.path.join("versions", archive_name)
-    exit_status = c.run("tar -cvzf {} web_static/".format(archive_path))
+    exit_status = local("tar -cvzf {} web_static/".format(archive_path))
     if exit_status.ok:
         return archive_path
     else:
