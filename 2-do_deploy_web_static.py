91a61f4ec130d9b16fcdca27897eb6405329f28a
aboubakr <c.aboubakr@gmail.com>
2024-01-10 20:22:33 +0100

    add the file 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from fabric.api import *
+from datetime import datetime
+import os
+"""module contains do_deploy function"""
+env.hosts = ['34.229.66.77', '18.209.225.222']
+
+
+@task
+def do_deploy(archive_path):
+    """distributes an archive to your web servers."""
+    success = True
+    current_directory = os.getcwd()
+    directory_path = os.path.join(current_directory, "versions")
+    if not os.path.exists(directory_path):
+        return False
+    archive_name = str(archive_path).split('/')[-1]
+    archive_basename = str(archive_path).split('/')[-1].split('.')[0]
+
+    for host in env.hosts:
+        result = put(archive_path, remote=f'/tmp/', host=host)
+        success = success and result.succeeded
+        result = run(f'mkdir /data/web_static/releases/{archive_basename}')
+        success = success and result.succeeded
+        result = run(f'tar -xzf /tmp/{archive_name}\
+            -C /data/web_static/releases/{archive_basename}')
+        success = success and result.succeeded
+        result = run(f'rm - rf /tmp/{archive_name}')
+        success = success and result.succeeded
+        result = run(f'rm /data/web_static/current')
+        success = success and result.succeeded
+        result = run(f'ln -sf /data/web_static/releases/{archive_basename}\
+                     /data/web_static/current')
+    return success
