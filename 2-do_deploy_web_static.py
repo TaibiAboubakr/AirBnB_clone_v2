c5e609a182fec28096df47a4553e41d06c6329d5
aboubakr <c.aboubakr@gmail.com>
2024-01-10 22:13:01 +0100

    update the file 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from fabric.api import *
-from datetime import datetime
+# distributes an archive to your web servers.
+from fabric.api import env, put, run
 import os
 """module contains do_deploy function"""
 env.hosts = ['34.229.66.77', '18.209.225.222']
@@ -8,23 +8,22 @@ env.hosts = ['34.229.66.77', '18.209.225.222']
 
 def do_deploy(archive_path):
     """distributes an archive to your web servers."""
-    success = True
     if os.path.isfile(archive_path) is False:
         return False
     archive_name = str(archive_path).split('/')[-1]
     archive_basename = str(archive_name).split('.')[0]
 
-    result = put(archive_path, remote=f'/tmp/')
-    success = success and result.succeeded
-    result = run(f'mkdir /data/web_static/releases/{archive_basename}')
-    success = success and result.succeeded
-    result = run(f'tar -xzf /tmp/{archive_name}\
-        -C /data/web_static/releases/{archive_basename}')
-    success = success and result.succeeded
-    result = run(f'rm - rf /tmp/{archive_name}')
-    success = success and result.succeeded
-    result = run(f'rm /data/web_static/current')
-    success = success and result.succeeded
-    result = run(f'ln -sf /data/web_static/releases/{archive_basename}\
+    if put(archive_path, f'/tmp/{archive_name}').failed:
+        return False
+    if run(f'mkdir /data/web_static/releases/{archive_basename}/').failed:
+        return False
+    if run(f'tar -xzf /tmp/{archive_name}\
+        -C /data/web_static/releases/{archive_basename}').failed:
+        return False
+    if run(f'rm - rf /tmp/{archive_name}').failed:
+        return False
+    if run(f'rm /data/web_static/current').failed:
+        return False
+    if run(f'ln -sf /data/web_static/releases/{archive_basename}\
                  /data/web_static/current')
-    return success
+    return True
