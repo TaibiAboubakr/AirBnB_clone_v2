a0138b9fbe988b1fd1fa6ae5dc65ac34ea1f0a94
aboubakr <c.aboubakr@gmail.com>
2024-01-11 23:50:10 +0100

    update the file 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,6 +31,30 @@ def do_deploy(archive_path):
         run("sudo rm -rf {}/web_static/".format(ver_to_deploy))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {}/ /data/web_static/current".format(ver_to_deploy))
+        content = """ server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /data/web_static/releases/test/;
+        index index.html;
+
+        location /redirect_me {
+            return 301 http://localhost/new_page;
+        }
+        error_page 404 /404.html;
+        location = /404.html {
+            internal;
+        }
+        location /hbnb_static/ {
+          alias /data/web_static/current/;
+       }
+        location / {
+          add_header X-Served-By "$(hostname)";
+       }
+    }
+    """
+        run("echo ($content) | sudo tee /etc/nginx/sites-available/default")
+        run("sudo service nginx reload")
         print("New version deployed!")
         return True
 
