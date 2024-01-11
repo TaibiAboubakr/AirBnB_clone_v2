01207d85d0079f977613f1d0237df7b2face3555
aboubakr <c.aboubakr@gmail.com>
2024-01-11 14:54:41 +0100

    add the file 3-deploy_web_static.py

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers
+"""
+
+from datetime import datetime
+from fabric.api import *
+import os
+
+env.hosts = ["44.210.150.159", "35.173.47.15"]
+env.user = "ubuntu"
+
+
+def do_pack():
+    """Generate a .tgz archive from the contents of the web_static folder."""
+    current_directory = os.getcwd()
+    directory_path = os.path.join(current_directory, "versions")
+    if not os.path.exists(directory_path):
+        local("mkdir -p versions")
+    timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    archive_name = "web_static_{}.tgz".format(timestamp)
+    print(timestamp)
+    archive_path = os.path.join("versions", archive_name)
+    exit_status = local("tar -cvzf {} web_static/".format(archive_path))
+    if exit_status.ok:
+        return archive_path
+    else:
+        return None
+
+
+def do_deploy():
+    """
+        Distribute archive.
+    """
+    archive_path = do_pack()
+    if os.path.exists(archive_path):
+        name = str(archive_path).split('/')[-1]
+        basename = str(name).split('.')[0]
+        ver_to_deploy = "/data/web_static/releases/" + basename
+        tmpname = "/tmp/" + name
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(ver_to_deploy))
+        run("sudo tar -xzf {} -C {}/".format(tmpname,
+                                             ver_to_deploy))
+        run("sudo rm {}".format(tmpname))
+        run("sudo mv {}/web_static/* {}".format(ver_to_deploy,
+                                                ver_to_deploy))
+        run("sudo rm -rf {}/web_static/".format(ver_to_deploy))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}/ /data/web_static/current".format(ver_to_deploy))
+        print("New version deployed!")
+        return True
+
+    return False
