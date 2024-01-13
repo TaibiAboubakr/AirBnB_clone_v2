487db88b31b04c1dd047798efbc862abadfbfbd0
aboubakr <c.aboubakr@gmail.com>
2024-01-13 16:19:05 +0100

    Revert "update the file 101-setup_web_static.pp"
    This reverts commit 936037fb7f0b659f86ea232be49a604343424c3a.

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,61 @@
 #site.pp
+exec { 'update':
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get update -y',
+  provider => shell,
+  
+}
 
-$nginx='
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  mode    => '0644',
+  content => template('nginx/default.erb'),
+  require => Service['nginx'],
+  notify  => Service['nginx'],
+}
+
+file { [ '/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test' ]:
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0644',
+  content => template('web_static/index.html.erb'),
+  require => File['/data/web_static/releases/test'],
+}
+
+file { '/data/web_static/releases/test/404.html':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0644',
+  content => 'Ceci n\'est pas une page',
+  require => File['/data/web_static/releases/test'],
+}
+
+file { '/data/web_static/current':
+  ensure  => link,
+  target  => '/data/web_static/releases/test',
+  require => File['/data/web_static/releases/test/index.html'],
+}
+
+# nginx/default.erb
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -24,70 +79,13 @@ server {
     location / {
       add_header X-Served-By {$hostname};
     }
-}'
+}
 
-$index='
+# web_static/index.html.erb
 <html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>'
-
-
-
-exec { 'update':
-  path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo apt-get update -y',
-  provider => shell,
-  
-}
-
-package { 'nginx':
-  ensure => installed,
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  mode    => '0644',
-  content => $nginx,
-  require => Service['nginx'],
-}
-
-file { [ '/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test' ]:
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
-}
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0644',
-  content => $index,
-  require => File['/data/web_static/releases/test'],
-}
-
-file { '/data/web_static/releases/test/404.html':
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0644',
-  content => 'Ceci n\'est pas une page',
-  require => File['/data/web_static/releases/test'],
-}
-
-file { '/data/web_static/current':
-  ensure  => link,
-  target  => '/data/web_static/releases/test',
-  require => File['/data/web_static/releases/test/index.html'],
-}
+</html>
