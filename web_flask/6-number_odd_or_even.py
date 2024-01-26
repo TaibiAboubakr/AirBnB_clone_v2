908cdffcbe21f8f15efc54342e347e12eb8ee20b
aboubakr <c.aboubakr@gmail.com>
2024-01-26 19:48:57 +0100

    update the file 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -12,15 +12,18 @@ def Home():
     """ Home page """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def Hbnb_page():
     """ Hbnb page """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_page(text):
     """ C page """
     return f"C {text}".replace("_", " ")
+
 
 @app.route('/python/(<text>)', strict_slashes=False)
 @app.route('/python', strict_slashes=False)
@@ -28,15 +31,18 @@ def python_page(text="is_cool"):
     """ Pyhton page """
     return f"Pyhton {text}".replace("_", " ")
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_page(n):
     """ Number page """
     return f"{n} is a number"
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template_page(n):
     """ Number template page """
     return render_template("5-number.html", num=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even_page(n):
