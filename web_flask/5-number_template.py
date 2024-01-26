04de736b6584c49dbda709c9f5f7651e3694d80f
aboubakr <c.aboubakr@gmail.com>
2024-01-26 19:48:54 +0100

    update the file 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -12,15 +12,18 @@ def Home():
     """ Home page """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def Hbnb_page():
     """ Hbnb page """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_page(text):
     """ C page """
     return f"C {text}".replace("_", " ")
+
 
 @app.route('/python/(<text>)', strict_slashes=False)
 @app.route('/python', strict_slashes=False)
@@ -28,10 +31,12 @@ def python_page(text="is_cool"):
     """ Pyhton page """
     return f"Pyhton {text}".replace("_", " ")
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_page(n):
     """ Number page """
     return f"{n} is a number"
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template_page(n):
